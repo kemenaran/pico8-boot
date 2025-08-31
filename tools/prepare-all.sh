64d0be8caa96763b0c8af624aa055b765a739bf6
Pierre de La Morinerie <kemenaran@gmail.com>
2025-08-31 16:36:26 +0200

    tools: add prepare-all

---
+++ tools/prepare-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -euxo pipefail
+#
+# Prepare all images of an animation for rendering by the main GBC executable:
+
+tools/prepare.sh 1
+tools/prepare.sh 2
+tools/prepare.sh 3
+tools/prepare.sh 4
+tools/prepare.sh 5
+tools/prepare.sh 6
+tools/prepare.sh 7
+tools/prepare.sh 8
+tools/prepare.sh 9
+tools/prepare.sh 10
